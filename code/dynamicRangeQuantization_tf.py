72dc35215d720263be9f5619bfd6de37317ddb99
AhmedFarrukh <af4232@nyu.edu>
2024-09-17 12:57:43 +0400

    added quantization code file

---
+++ code/dynamicRangeQuantization_tf.py
@@ -0,0 +1,44 @@
+#main dependency: TF version 2.17.0
+
+import tensorflow as tf
+import os
+import pathlib
+import argparse
+import sys
+
+modelNames = ["MobileNet", "InceptionV3", "ResNet50", "ResNet101", "ResNet152", "VGG16", "VGG19"]
+
+def quantize(dir):
+    for modelName in modelNames:
+        model_class = getattr(tf.keras.applications, modelName)
+        model = model_class(weights='imagenet')
+
+        converter = tf.lite.TFLiteConverter.from_keras_model(model)
+        tflite_model = converter.convert()
+
+        converter = tf.lite.TFLiteConverter.from_keras_model(model)
+        converter.optimizations = [tf.lite.Optimize.DEFAULT]
+        tflite_model_quant = converter.convert()
+
+        tflite_models_dir = pathlib.Path(dir)
+        tflite_models_dir.mkdir(exist_ok=True, parents=True)
+
+        # Save the unquantized/float model:
+        tflite_model_file = tflite_models_dir/(modelName+".tflite")
+        tflite_model_file.write_bytes(tflite_model)
+        # Save the quantized model:
+        tflite_model_quant_file = tflite_models_dir/(modelName+"_quant.tflite")
+        tflite_model_quant_file.write_bytes(tflite_model_quant)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--dir', help='The directory to save the models', required=True)
+    
+    args = parser.parse_args()
+
+    if not args.dir:
+        print("Warning: Must provide: directory.")
+        sys.exit(1)
+
+    quantize(args.dir)
