5527df3b815f8cf05928ce7bb2951ca041a63d7c
AhmedFarrukh <af4232@nyu.edu>
2024-10-07 21:43:22 +0400

    updated onnx modelnames

--- code/dynamicRangeQuantization_onnx.py
+++ code/dynamicRangeQuantization_onnx.py
@@ -5,7 +5,7 @@ from onnx import hub
 import pathlib
 import argparse
 
-modelNames = ["mobilenetv2-10", "inception_v2", "resnet50_v1", "resnet101_v1", "resnet152_v1", "vgg16", "vgg19"]
+modelNames = ["mobilenet", "inception_v2", "resnet50", "resnet101", "resnet152", "vgg16", "vgg19"]
 
 def quantize(save_dir):
     onnx_models_dir = pathlib.Path(save_dir)
