b8f3f282c2caaa7919d26d3c28c0b95655c8ac9f
AhmedFarrukh <af4232@nyu.edu>
2024-10-28 21:54:36 +0400

    edited onnx load method

--- code/dynamicRangeQuantization_onnx.py
+++ code/dynamicRangeQuantization_onnx.py
@@ -1,23 +1,40 @@
 import os
 import onnx
 from onnxruntime.quantization import quantize_dynamic, QuantType
-from onnx import hub
 import pathlib
 import argparse
+import requests
 
-modelNames = ["resnet50", "inception_v2", "mobilenet", "resnet101", "resnet152", "vgg16", "vgg19"]
+modelNames = {"MobileNet": "https://github.com/onnx/models/raw/main/Computer_Vision/mobilenetv2_100_Opset16_timm/mobilenetv2_100_Opset16.onnx",
+              "InceptionV3": "https://github.com/onnx/models/raw/main/Computer_Vision/inception_v3_Opset17_timm/inception_v3_Opset17.onnx",
+              "ResNet50": "https://github.com/onnx/models/raw/main/Computer_Vision/resnet50_Opset16_timm/resnet50_Opset16.onnx",
+              "ResNet101": "https://github.com/onnx/models/raw/main/Computer_Vision/resnet101_Opset17_torch_hub/resnet101_Opset17.onnx", 
+              "ResNet152": "https://github.com/onnx/models/raw/main/Computer_Vision/resnet152_Opset17_timm/resnet152_Opset17.onnx",
+               "VGG16": "https://github.com/onnx/models/raw/main/Computer_Vision/vgg16_Opset16_timm/vgg16_Opset16.onnx", 
+               "VGG19": "https://github.com/onnx/models/raw/main/Computer_Vision/vgg19_Opset16_timm/vgg19_Opset16.onnx"}
 
 def quantize(save_dir):
     onnx_models_dir = pathlib.Path(save_dir)
     onnx_models_dir.mkdir(exist_ok=True, parents=True)
 
     for modelName in modelNames:
-        model = hub.load(modelName)
-
         original_model_path = os.path.join(save_dir, f"{modelName}.onnx")
         quantized_model_path = os.path.join(save_dir, f"{modelName}_quant.onnx")
 
-        onnx.save(model, original_model_path)
+        try:
+            # Send a GET request to the URL
+            response = requests.get(modelNames[modelName], stream=True)
+            response.raise_for_status()  # Raise an exception for HTTP errors
+            
+            # Write the content to a file
+            with open(original_model_path, "wb") as file:
+                for chunk in response.iter_content(chunk_size=8192):
+                    file.write(chunk)
+            print(f"File downloaded successfully as {original_model_path}")
+        except requests.exceptions.RequestException as e:
+            print(f"An error occurred: {e}")
+
+        model = onnx.load(original_model_path)
 
         # Perform dynamic range quantization
         quantize_dynamic(
